899faa064874d7db8a786fd911b03c52549e2f42
Donald Donchi Fofack <donald.donchi.fofack@mni.thm.de>
2021-12-28 17:24:48 +0100

    create methode image_to_array

--- src/load_image_and_predict.py
+++ src/load_image_and_predict.py
@@ -1,6 +1,16 @@
+import cv2 as opencv
+import numpy as np
+
 import tensorflow.keras as keras
 
 #Load the model
 model = keras.models.load_model('../model/ziffer_model.h5')
+
+
+def image_to_array(image_name):
+    img_to_array = opencv.imread(image_name)[:, :, 0]  # shape(28 x 28 x 3 )(RGB) to shape (28 x 28) (BW)
+    img_to_array_invert = np.invert(np.array([img_to_array]))  # shwarz to weiß und weiß to shwarz
+    return img_to_array_invert
+
 
 
