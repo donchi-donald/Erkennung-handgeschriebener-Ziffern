05a11b16c6c7d5fad2b742c6865da764c8cdbfdd
Donald Donchi Fofack <donald.donchi.fofack@mni.thm.de>
2021-12-29 16:36:33 +0100

    last update

--- src/load_image_and_predict.py
+++ src/load_image_and_predict.py
@@ -12,15 +12,16 @@ model = keras.models.load_model('../model/ziffer_model.h5')
 def image_to_array(image_name):
     img_to_array = opencv.imread(image_name)[:, :, 0]  # shape(28 x 28 x 3 )(RGB) to shape (28 x 28) (BW)
     img_to_array_invert = np.invert(np.array([img_to_array]))  # shwarz to weiß und weiß to shwarz
+    img_to_array_invert = img_to_array_invert / 255.
     return img_to_array_invert
 
 
 def show_image(image_array):
-    plt.imshow(image_array[0], cmap=plt.cm.binary)
+    plt.imshow(np.reshape(image_array, [28,28]), cmap='gray')
     plt.show()
 
 
-i = 0
+i = 5
 while True:
     image_name = '../img/ziffer{}.png'.format(i)
     isImg = os.path.isfile(image_name)
